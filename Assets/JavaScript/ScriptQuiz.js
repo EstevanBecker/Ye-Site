ee3b46ef9873a32ba35804cb47720fc8f78b6cae
EstevanBecker <estevansaldanhadossantos@gmail.com>
2022-06-18 22:20:05 -0300

    Ye

---
+++ Assets/JavaScript/ScriptQuiz.js
@@ -0,0 +1,32 @@
+var YeQuestions = [
+    {
+        question: "Qual a idade de Ye?",
+        answers: {
+            a: "30",
+            b: "40",
+            c: "44",
+            d: "45"
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: 'qual o primeiro album lançado por Kanye West?',
+        answers: {
+            a: 'My Beautiful Dark Twisted Fantasy',
+            b: 'Graduation',
+            c: 'Donda',
+            d: 'College Dropout'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: 'Ye',
+        answers: {
+            a: 'Ye',
+            b: 'ye',
+            c: 'yE',
+            d: 'YE'
+        },
+        correctAnswer: 'a'
+    },
+];
