6479c78cc5ce68544313e9aca7d12c9bb1fd71ff
EstevanBecker <estevansaldanhadossantos@gmail.com>
2022-07-06 13:41:03 -0300

    ye

--- Assets/JavaScript/ScriptQuiz.js
+++ Assets/JavaScript/ScriptQuiz.js
@@ -1,4 +1,4 @@
-var yeQuestions = [
+const yeQuestions = [
     {
         question: "Qual a idade de Ye?",
         answers: {
@@ -40,7 +40,7 @@ var yeQuestions = [
         correctAnswer: 'd'
     },
     {
-        question: 'Kanye West ficou muito famoso ao popularizar um certo instrumento para auxilio vocal muito utilizado hoje em dia, qual o nome deste instrumento?',
+        question: 'Kanye West ficou muito famoso ao popularizar uma certa ferramenta para auxilio vocal muito utilizado hoje em dia, qual o nome dessa ferramenta',
         answers: {
             a: 'gaita de boca',
             b: 'Autotune',
@@ -51,12 +51,90 @@ var yeQuestions = [
     }
 ];
 
-const quizContainer = document.getElementById ('quiz')
-const resultadoContainer = document.getElementById ('results')
-const submitButton = document.getElementById ('submit')
- 
-generateQuiz(yeQuestions, quizContainer, resultadoContainer, submitButton) {
-    function showQuestions (yeQuestions, quizContainer)
-    
-}
+const quizContainer = document.getElementById('quiz')
+const resultadoContainer = document.getElementById('results')
+const submitButton = document.getElementById('submit')
 
+generateQuiz(yeQuestions, quizContainer, resultadoContainer, submitButton)
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+    function showQuestions(questions, quizContainer) {
+        // precisaremos de um lugar para armazenar a saída e as opções de resposta
+        var output = [];
+        var answers; // poderia ter dado outro nome, alem do mais, eu mesmo me confundi :/
+
+        // para cada questão
+        for (var i = 0; i < questions.length; i++) {
+
+            // mas, primeiro resetamos a lista de questões
+            answers = [];
+
+            // e aqui faremos para cada resposta na questão.
+            for (letter in questions[i].answers) {
+
+                // Aqui será escrito para html para usar o Radio.
+                answers.push(
+                    '<label>'
+                    + '<input type="radio" name="question' + i + '" value="' + letter + '">'
+                    //	+ letter + ') ' vou deixar a letra comentada, porque achei melhor assim
+                    + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer) {
+
+
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+
+
+        var userAnswer = '';
+        var numCorrect = 0;
+
+
+        for (var i = 0; i < questions.length; i++) {
+
+            // agora ele vai virar a letra que esta marcada.
+            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+
+
+            if (userAnswer === questions[i].correctAnswer) {
+
+                numCorrect++;
+
+
+                answerContainers[i].style.color = 'lightgreen';
+            }
+
+            else {
+
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+
+        resultsContainer.innerHTML = numCorrect + ' de ' + questions.length;
+    }
+
+    // show questions right away
+    showQuestions(questions, quizContainer);
+
+    // on submit, show results
+    submitButton.onclick = function () {
+        showResults(questions, quizContainer, resultsContainer);
+    }
+}
