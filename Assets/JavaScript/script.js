a62534804c288f81701604c263a06416d8606d1c
EstevanBecker <estevansaldanhadossantos@gmail.com>
2022-07-06 13:44:38 -0300

    ye

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,29 +2,5 @@ function funcao2() {
     document.write("Marcelos");
 }
 
-//console.log ("mensagem");
-//console.log (1);
-//let: valor = "Francisco";
-//console.log(valor);
+const yeMenu = document.getElementById ('menu')
 
-//console.warn(2);
-//console.error(3);
-
-//let: valor = "Francisco";
-//alert (1);
-//alert("mensagem");
-//alert (valor);
-
-//let: valor = prompt ("Qual o Seu nome?")
-//alert (valor);
-
-//Alert ("Olá Tudo bem?", valor);
-
-//let: idade = confirm ("Você tem mais de 18 anos?");
-//console.log (idade);
-
-//let numero = prompt ("Escolha um número:");
-
-//while. (numero*numero != prompt ("Qual a multiplicação desse número por ele mesmo?")) {
-//
-//};
